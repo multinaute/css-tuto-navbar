d8ae051eb33b79e3cdd21cbd441a968b8d515064
Sow Yet <multinaute@gmail.com>
2021-08-19 19:04:56 +0200

    add layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,56 +1,11 @@
-import React from "react"
-import { Link } from "gatsby"
-import { Helmet } from "react-helmet"
-
-// data
-const links = [
-  {
-    text: "Home",
-    url: "#",
-  },
-  {
-    text: "About",
-    url: "#",
-  },
-  {
-    text: "Shop",
-    url: "#",
-  },
-  {
-    text: "Blog",
-    url: "#",
-  },
-  {
-    text: "Connect",
-    url: "#",
-  },
-  {
-    text: "Contact",
-    url: "#",
-  },
-]
+import * as React from "react"
+import Layout from "../compoments/layout"
 
 function index() {
   return (
-    <>
-      <Helmet>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap"
-          rel="stylesheet"
-        />
-      </Helmet>
-      <nav>
-        <ul>
-          {links.map(link => (
-            <li key={link.url}>
-              <Link to={link.url}>{link.text}</Link>
-            </li>
-          ))}
-        </ul>
-      </nav>
-    </>
+    <Layout pageTitle="Home page">
+      <p>home page</p>
+    </Layout>
   )
 }
 
